4ee2a407fcfbae52612c60aa15a62f8883fb9066
Baili Deng <rayman0411@gmail.com>
2018-10-28 04:27:07 +0800

    Added  a bunch of testing images

--- main.py
+++ main.py
@@ -2,39 +2,47 @@ import cv2 as cv
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+import matplotlib.pyplot as plt
+
 
 cap = cv.VideoCapture(0)
 bg = cv.createBackgroundSubtractorMOG2(10, 20)
 detect = False
 right_sided = True
-count = 187
+count = 264
+cnt = 0
 
 feature = []
 target = []
-for i in range(187):
-    feature.append(cv.read('hand_five' + i + '.jpg'))
+
+for i in range(count):
+    feature.append(cv.imread('hand_five' + str(i) + '.jpg',cv.IMREAD_GRAYSCALE))
     target.append(0)
-for i in range(187):
-    feature.append(cv.read('hand_thumbs_up' + i + '.jpg'))
+for i in range(count):
+    feature.append(cv.imread('hand_thumbs_up' + str(i) + '.jpg',cv.IMREAD_GRAYSCALE))
     target.append(1)
-for i in range(187):
-    feature.append(cv.read('hand_victory' + i + '.jpg'))
+for i in range(count):
+    feature.append(cv.imread('hand_nothing' + str(i) + '.jpg',cv.IMREAD_GRAYSCALE))
     target.append(2)
-for i in range(187):
-    feature.append(cv.read('hand_ok_hand' + i + '.jpg'))
-    target.append(3)
+h,w = feature[0].shape
+
+feature = np.asarray(feature)
+print(feature.shape)
+target = np.asarray(target)
+feature = feature / 255
 
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
+    keras.layers.Flatten(input_shape=(h,w)),
+    keras.layers.Dense(16, activation=tf.nn.relu),
+    keras.layers.Dense(16, activation=tf.nn.relu),
+    keras.layers.Dense(3, activation=tf.nn.softmax)
 ])
 
-model.compile(optimizer=tf.train.AdamOptimizer(),
+model.compile(optimizer=tf.train.AdamOptimizer(), 
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=5)
+model.fit(feature, target, epochs=2)
 
 while(True):
     _, frame = cap.read()
@@ -61,13 +69,17 @@ while(True):
     black = np.zeros((height // 4 * 3, width // 5 * 2, 3))
     cv.drawContours(black, contours, -1, (255, 255, 255), 3)
     cv.imshow('frame', black)
-
-    predictions = model.predict([black])
-    print(np.argmax(predictions[0]))
-
+    
     k = cv.waitKey(1)
     if k == ord('w'):
         detect = not detect
+    if k == ord('f'):
+        #cv.imwrite('hand_nothing'+str(cnt)+'.jpg',black)
+        cnt = cnt+1
+        black = np.uint8(black)
+        black=cv.cvtColor(black,code=cv.COLOR_RGB2GRAY)
+        predictions = model.predict(np.asarray([black]))
+        print(np.argmax(predictions[0]))
     if k == 27:
         break
 
